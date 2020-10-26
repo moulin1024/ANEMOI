663eeda64409ddc55805761cd8fe6c39f4c9a4c6
Mou Lin <moulin1024@gmail.com>
2020-10-26 18:05:08 +0100

    python script for rotating the wind farm

---
+++ rotate.py
@@ -0,0 +1,19 @@
+import os
+import numpy as np
+import pandas as pd
+import sys
+theta = sys.argv[1]
+
+theta = np.radians(np.double(theta))
+
+wind_df = pd.read_csv("turbine_loc.dat")
+x = wind_df["x"]
+y = wind_df["y"]
+
+x_rotate = x[0] + (x-x[0])*np.cos(theta) - (y-y[0]) * np.sin(theta)
+y_rotate = y[0] + (y-y[0])*np.cos(theta) + (x-x[0]) * np.sin(theta)
+
+wind_df["x"] = x_rotate
+wind_df["y"] = y_rotate
+wind_df.to_csv("turb_loc.dat",index=False)
+
