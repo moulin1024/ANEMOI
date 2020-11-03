e864be310683c873f7fdea244f4a4531a5ca1188
Mou Lin <moulin1024@gmail.com>
2020-11-03 16:29:34 +0100

    A working turbine for multi case (without tower)

---
+++ para_explore.sh
@@ -0,0 +1,9 @@
+./debug_run.sh warmup-fine
+./debug_run.sh pre-fine
+./debug_run.sh main-fine
+# wireles anime main-fine
+# cd post/haohua
+# cp ../../job/main-fine/output/main-fine_ta.h5 ./sim/
+# python postprocessing.py
+# cd ..
+# cd ..
