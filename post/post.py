3c5e4d71bdb4d75cff2d5787a9595df50e51b000
Mou Lin <moulin1024@gmail.com>
2021-11-17 16:29:12 +0100

    Improved animation post processing

--- post/post.py
+++ post/post.py
@@ -4,44 +4,31 @@ import pandas as pd
 import fatpack
 import fatigue
 
-root_moment_flap0 = pd.read_csv('../job/NREL-m/src/output/root_moment.csv',header=None).iloc[0:-1].astype(float).to_numpy()
-root_moment_flap1 = pd.read_csv('../job/NREL-m-yaw1/src/output/root_moment.csv',header=None).iloc[0:-1].astype(float).to_numpy()
-root_moment_flap2 = pd.read_csv('../job/NREL-m-yaw2/src/output/root_moment.csv',header=None).iloc[0:-1].astype(float).to_numpy()
-
-root_moment_flap0 = root_moment_flap0[2000:3000]
-root_moment_flap1 = root_moment_flap1[2000:3000]
-root_moment_flap2 = root_moment_flap2[2000:3000]
-
-# root_force_flap  = pd.read_csv('../job/NREL-m/src/output/root.csv',header=None).iloc[0:-1].astype(float).to_numpy()
-# root_force_flap2 = pd.read_csv('../job/NREL-m-yaw1/src/output/root.csv',header=None).iloc[0:-1].astype(float).to_numpy()
-# root_force_flap0 = pd.read_csv('../job/NREL-m-yaw2/src/output/root.csv',header=None).iloc[0:-1].astype(float).to_numpy()
-
-# print(np.mean(root_force_flap[10000:]))
-# print(np.mean(root_force_flap2[10000:]))
-# print(np.mean(root_force_flap0[10000:]))
+root_moment0 = pd.read_csv('../job/NREL-m/output/root_moment.csv',header=None).iloc[0:-1].astype(float).to_numpy()
+root_moment1 = pd.read_csv('../job/NREL-m-yaw1/output/root_moment.csv',header=None).iloc[0:-1].astype(float).to_numpy()
+root_moment2 = pd.read_csv('../job/NREL-m-yaw2/output/root_moment.csv',header=None).iloc[0:-1].astype(float).to_numpy()
 
 fig,ax = plt.subplots(1,1)
-plt.plot(root_moment_flap0,lw=1)
-plt.plot(root_moment_flap1,lw=1)
-plt.plot(root_moment_flap2,lw=1)
-# plt.xlim([8000,10000])
-# plt.xlim([1000,3000])
-# plt.ylim([4e6,7e6])
-# fig,ax = plt.subplots(1,1)
-# plt.rcParams['image.cmap']='Purples'
-# value_plot = u[0,128,:,:].T
-# plt.contourf(value_plot,100)
-# ax.set_aspect(0.5)
+plt.plot(root_moment0[:,0],lw=1)
+plt.plot(root_moment1[:,0],lw=1)
+plt.plot(root_moment2[:,0],lw=1)
+plt.xlim([8000,10000])
+plt.savefig('flap_moment.png')
 
-# plt.clim(np.amin(value_plot),np.amax(value_plot)*1.6)
-plt.savefig('moment.png')
+
+fig,ax = plt.subplots(1,1)
+plt.plot(root_moment0[:,1],lw=1)
+plt.plot(root_moment1[:,1],lw=1)
+plt.plot(root_moment2[:,1],lw=1)
+plt.xlim([8000,10000])
+plt.savefig('edge_moment.png')
 
 
 # fig,ax = plt.subplots(1,1)
 # plt.plot(root_force_flap)
 # plt.plot(root_force_flap2)
 # plt.plot(root_force_flap0)
-# # plt.xlim([8000,10000])
+plt.xlim([8000,10000])
 # # plt.ylim([4e6,7e6])
 # # fig,ax = plt.subplots(1,1)
 # # plt.rcParams['image.cmap']='Purples'
@@ -51,10 +38,10 @@ plt.savefig('moment.png')
 
 # # plt.clim(np.amin(value_plot),np.amax(value_plot)*1.6)
 # plt.savefig('force.png')
-Neq = 100
-M_eq_baseline = fatigue.get_DEL(root_moment_flap0,Neq,10)
+Neq = 1000
+M_eq_baseline = fatigue.get_DEL(root_moment0[:,0],Neq,10)
 print(M_eq_baseline)
-M_eq_baseline = fatigue.get_DEL(root_moment_flap1,Neq,10)
+M_eq_baseline = fatigue.get_DEL(root_moment1[:,0],Neq,10)
 print(M_eq_baseline)
-M_eq_baseline = fatigue.get_DEL(root_moment_flap2,Neq,10)
+M_eq_baseline = fatigue.get_DEL(root_moment2[:,0],Neq,10)
 print(M_eq_baseline)
--- prc/app_anime.py
+++ prc/app_anime.py
@@ -25,6 +25,8 @@ import pandas as pd
 from pathlib import Path
 from matplotlib.pyplot import figure
 from matplotlib import animation, rc
+from mpl_toolkits.mplot3d import Axes3D
+import fatigue
 # from pyevtk.hl import gridToVTK
 
 
@@ -61,6 +63,10 @@ def anime(PATH, case_name):
 
     space = post.get_space(config)
     time = post.get_time(config)
+    print(case_path)
+
+    df = pd.read_csv(case_path+"/input/turb_loc.dat")
+    # print(df['yaw'][0])
 
     if config['ts_flag'] > 0:
         result_4d = post.get_result_4d(src_out_path, config)
@@ -85,23 +91,17 @@ def anime(PATH, case_name):
         XX3,ZZ3 = np.meshgrid(x,z)
 
 
-        fig,ax = plt.subplots(1,1)
-        # plt.rcParams['image.cmap']='Greys'
-        def animate(i):    #     azimuths = np.radians(np.linspace(0, 360, 40))
-            # values = w[i,:,:,4]#np.random.random((azimuths.size, zeniths.size))
-            values = w[i,:,128,:]#np.random.random((azimuths.size, zeniths.size))
-            plt.cla()
-            # im1 = ax.imshow(values.T,origin='lower',aspect=config['dz']/config['dy'])
-            im1 = ax.imshow(values.T,origin='lower',aspect=config['dz']/config['dx'])
-            # plt.clim(0,10000)
-            ax.set_xlabel('x')
-            ax.set_ylabel('y')
-            # plt.xlim([100,150])
-            # plt.ylim([0,100])
-            print(i)
-            return
-        anim = animation.FuncAnimation(fig, animate, frames=10)
-        anim.save(out_path+'/animation_xz.gif',writer='pillow', fps=20)
+        # fig,ax = plt.subplots(1,1)
+        # def animate(i):    
+        #     values = u[i,:,:,45]
+        #     plt.cla()
+        #     im1 = ax.imshow(values.T,origin='lower',aspect=config['dx']/config['dy'])
+        #     ax.set_xlabel('x')
+        #     ax.set_ylabel('y')
+        #     print(i)
+        #     return
+        # anim = animation.FuncAnimation(fig, animate, frames=10)
+        # anim.save(out_path+'/animation_xz.gif',writer='pillow', fps=10)
 
     if config['turb_flag'] > 0:
         turb_force = post.get_turb(src_out_path, config)
@@ -112,78 +112,107 @@ def anime(PATH, case_name):
         turb_y = 512 - config['dy']
         turb_z = 90
 
-        downsample = 100
+        downsample = 10
         flap_scale = 1
-        edge_scale = 40
+        edge_scale = 50
         displacement_flap = turb_force['displacement_flap']
         displacement_edge = turb_force['displacement_edge']
         moment_flap = turb_force['moment_flap']
         moment_edge = turb_force['moment_edge']
 
+        print(moment_edge.shape)
 
+        initial_phase = [0,-2*np.pi/3,-4*np.pi/3]
+        blade_coord = np.zeros([32,3,3])
+        blade_coord_tilted = np.zeros([32,3,3])
+        blade_coord_yawed = np.zeros([32,3,3])
+        blade = np.linspace(1.5,63,32)
         omega = 12.1/69*2*np.pi
         phase_angle = 0
-        fig,ax = plt.subplots()
-        # ax = fig.add_subplot(1,2,1)
-        def animate(i):
-            ax.cla() # clear the previous image
-            # ax[1,0].cla() # clear the previous image
-            # ax[0,1].cla() # clear the previous image
-            # ax[1,1].cla() # clear the previous image
-            blade = np.linspace(1.5,63,32)
-            phase_angle = (i+1)*downsample*omega*config['dtr']
-            y_coord_1 = turb_y + blade*np.cos(phase_angle) - displacement_edge[(i+1)*downsample,0,:,0]*edge_scale*np.sin(phase_angle)
-            z_coord_1 = turb_z + blade*np.sin(phase_angle) + displacement_edge[(i+1)*downsample,0,:,0]*edge_scale*np.cos(phase_angle)
-            x_coord_1 = turb_x + displacement_flap[i,0,:,0]*flap_scale
-            y_coord_2 = turb_y + blade*np.cos(phase_angle-2*np.pi/3) - displacement_edge[(i+1)*downsample,1,:,0]*edge_scale*np.sin(phase_angle-2*np.pi/3)
-            z_coord_2 = turb_z + blade*np.sin(phase_angle-2*np.pi/3) + displacement_edge[(i+1)*downsample,1,:,0]*edge_scale*np.cos(phase_angle-2*np.pi/3)
-            x_coord_2 = turb_x + displacement_flap[i,1,:,0]*flap_scale
-            y_coord_3 = turb_y + blade*np.cos(phase_angle-4*np.pi/3) - displacement_edge[(i+1)*downsample,2,:,0]*edge_scale*np.sin(phase_angle-4*np.pi/3)
-            z_coord_3 = turb_z + blade*np.sin(phase_angle-4*np.pi/3) + displacement_edge[(i+1)*downsample,2,:,0]*edge_scale*np.cos(phase_angle-4*np.pi/3)
-            x_coord_3 = turb_x + displacement_flap[i,2,:,0]*flap_scale
+        tilt_angle = np.deg2rad(df['tilt'][0])
+        yaw_angle = np.deg2rad(df['yaw'][0])
+        
+        # total_thrust = np.sum(np.sum(np.sum(turb_fx,axis=-1),axis=-1),axis=-1)
+        # plt.plot(moment_flap[:,0,0,0])
+        # plt.plot(moment_flap[:,1,0,0])
+        # plt.plot(moment_flap[:,2,0,0])
+        # plt.savefig(out_path+'/total force.png')
+        
+        Neq = 1000
+        M_eq_baseline = fatigue.get_DEL(moment_edge[20000:,0,0,0],Neq,10)
+        print(M_eq_baseline)
+        M_eq_baseline = fatigue.get_DEL(moment_edge[20000:,1,0,0],Neq,10)
+        print(M_eq_baseline)
+        M_eq_baseline = fatigue.get_DEL(moment_edge[20000:,2,0,0],Neq,10)
+        print(M_eq_baseline)
 
-            values = u[i,128,:,:]
-            ax.plot(y_coord_1,z_coord_1,'r')
-            ax.plot(y_coord_2,z_coord_2,'r')
-            ax.plot(y_coord_3,z_coord_3,'r')
-            ax.contourf(YY2,ZZ2,values.T,100)
-            ax.axis('scaled')
-            ax.set_ylim([0,200]) # fix the x axis
-            ax.set_xlim([turb_y-200,turb_y+200]) # fix the y axis
+        arr2D = np.array([moment_flap[:,0,0,0],moment_edge[:,0,0,0]]).T
+        np.savetxt(out_path + '/root_moment.csv', arr2D, delimiter=',', fmt='%d')
+        # # print(turb_ft)
+
+
+        # fig = figure()
+        # ax1 = fig.add_subplot(221)
+        # ax2 = fig.add_subplot(222)
+        # ax3 = fig.add_subplot(223)
+        # ax4 = fig.add_subplot(224,projection='3d')
+
+        # def animate(i):
+
+        #     ax1.cla() # clear the previous image
+        #     ax2.cla() # clear the previous image
+        #     ax3.cla() # clear the previous image
+        #     ax4.cla() # clear the previous image
+        #     phase_angle = (i+1)*downsample*omega*config['dtr']
+        #     for j in range(3):
+        #         blade_coord[:,j,0] = displacement_flap[(i+1)*downsample,j,:,0]*flap_scale
+        #         blade_coord[:,j,1] = blade*np.cos(phase_angle+initial_phase[j]) - displacement_edge[(i+1)*downsample,j,:,0]*edge_scale*np.sin(phase_angle+initial_phase[j])
+        #         blade_coord[:,j,2] = blade*np.sin(phase_angle+initial_phase[j]) + displacement_edge[(i+1)*downsample,j,:,0]*edge_scale*np.cos(phase_angle+initial_phase[j])
+                
+        #         blade_coord_tilted[:,j,0] = blade_coord[:,j,0]*np.cos(tilt_angle) + blade_coord[:,j,2]*np.sin(tilt_angle)
+        #         blade_coord_tilted[:,j,1] = blade_coord[:,j,1]
+        #         blade_coord_tilted[:,j,2] = blade_coord[:,j,2]*np.cos(tilt_angle) - blade_coord[:,j,0]*np.sin(tilt_angle)
+                
+        #         blade_coord_yawed[:,j,0] = blade_coord_tilted[:,j,0]*np.cos(yaw_angle) - blade_coord_tilted[:,j,1]*np.sin(yaw_angle)  
+        #         blade_coord_yawed[:,j,1] = blade_coord_tilted[:,j,1]*np.cos(yaw_angle) + blade_coord_tilted[:,j,0]*np.sin(yaw_angle) 
+        #         blade_coord_yawed[:,j,2] = blade_coord_tilted[:,j,2] 
+
+
+        #         blade_coord_yawed[:,j,0] = turb_x + blade_coord_yawed[:,j,0]
+        #         blade_coord_yawed[:,j,1] = turb_y + blade_coord_yawed[:,j,1]
+        #         blade_coord_yawed[:,j,2] = turb_z + blade_coord_yawed[:,j,2]
+
+        #         ax1.plot(blade_coord_yawed[:,j,1], blade_coord_yawed[:,j,2])
+        #         ax2.plot(blade_coord_yawed[:,j,0], blade_coord_yawed[:,j,2])
+        #         ax3.plot(blade_coord_yawed[:,j,0], blade_coord_yawed[:,j,1])
+        #         ax4.plot3D(blade_coord_yawed[:,j,0],blade_coord_yawed[:,j,1], blade_coord_yawed[:,j,2])
+
+        #     ax1.axis('scaled')
+        #     ax1.set_xlabel('y (m)')
+        #     ax1.set_ylabel('z (m)')
+        #     ax1.set_ylim([0,200]) # fix the x axis
+        #     ax1.set_xlim([turb_y-100,turb_y+100]) # fix the y axis
             
-            print(i)
+        #     ax2.axis('scaled')
+        #     ax2.set_xlabel('x (m)')
+        #     ax2.set_ylabel('z (m)')
+        #     ax2.set_ylim([0,200]) # fix the x axis
+        #     ax2.set_xlim([turb_x-100,turb_x+100]) # fix the y axis
 
-        anim = animation.FuncAnimation(fig, animate, frames=10)
-        anim.save(out_path+'/blade_movement_yz.gif',writer='pillow', fps=12)
+        #     ax3.axis('scaled')
+        #     ax3.set_xlabel('x (m)')
+        #     ax3.set_ylabel('z (m)')
+        #     ax3.set_ylim([turb_y-100,turb_y+100]) # fix the x axis
+        #     ax3.set_xlim([turb_x-100,turb_x+100]) # fix the y axis
 
-        fig,ax = plt.subplots()
-        def animate(i):
-            ax.cla() # clear the previous image
-            # ax[1,0].cla() # clear the previous image
-            # ax[0,1].cla() # clear the previous image
-            # ax[1,1].cla() # clear the previous image
-            blade = np.linspace(1.5,63,32)
-            phase_angle = (i+1)*downsample*omega*config['dtr']
-            y_coord_1 = turb_y + blade*np.cos(phase_angle) - displacement_edge[(i+1)*downsample,0,:,0]*edge_scale*np.sin(phase_angle)
-            z_coord_1 = turb_z + blade*np.sin(phase_angle) + displacement_edge[(i+1)*downsample,0,:,0]*edge_scale*np.cos(phase_angle)
-            x_coord_1 = turb_x + displacement_flap[i,0,:,0]*flap_scale
-            y_coord_2 = turb_y + blade*np.cos(phase_angle-2*np.pi/3) - displacement_edge[(i+1)*downsample,1,:,0]*edge_scale*np.sin(phase_angle-2*np.pi/3)
-            z_coord_2 = turb_z + blade*np.sin(phase_angle-2*np.pi/3) + displacement_edge[(i+1)*downsample,1,:,0]*edge_scale*np.cos(phase_angle-2*np.pi/3)
-            x_coord_2 = turb_x + displacement_flap[i,1,:,0]*flap_scale
-            y_coord_3 = turb_y + blade*np.cos(phase_angle-4*np.pi/3) - displacement_edge[(i+1)*downsample,2,:,0]*edge_scale*np.sin(phase_angle-4*np.pi/3)
-            z_coord_3 = turb_z + blade*np.sin(phase_angle-4*np.pi/3) + displacement_edge[(i+1)*downsample,2,:,0]*edge_scale*np.cos(phase_angle-4*np.pi/3)
-            x_coord_3 = turb_x + displacement_flap[i,2,:,0]*flap_scale
+        #     ax4.set_xlabel('x (m)')
+        #     ax4.set_ylabel('y (m)')
+        #     ax4.set_zlabel('z (m)')
+        #     ax4.set_zlim([0,200]) # fix the x axis
+        #     ax4.set_xlim([turb_x-100,turb_x+100]) # fix the y axis
+        #     ax4.set_ylim([turb_y-100,turb_y+100]) # fix the y axis
 
-            values = w[i,:,128,:]
-            ax.plot(x_coord_1,z_coord_1,'r')
-            ax.plot(x_coord_2,z_coord_2,'r')
-            ax.plot(x_coord_3,z_coord_3,'r')
-            ax.contourf(XX3,ZZ3,values.T,100)
-            ax.axis('scaled')
-            ax.set_ylim([0,200]) # fix the x axis
-            ax.set_xlim([turb_y-200,turb_y+200]) # fix the y axis
-            
-            print(i)
+        #     print(i)
 
-        anim = animation.FuncAnimation(fig, animate, frames=10)
-        anim.save(out_path+'/blade_movement.gif',writer='pillow', fps=12)
+        # anim = animation.FuncAnimation(fig, animate, frames=10)
+        # anim.save(out_path+'/blade_movement_yz.gif',writer='pillow', fps=12)
