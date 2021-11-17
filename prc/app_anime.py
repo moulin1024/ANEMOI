f4ad8e514f2b083d57b2531c099e6b9726ebbaf0
Mou Lin <moulin1024@gmail.com>
2021-11-17 23:18:53 +0100

    Again fix the gravity force problem

--- prc/app_anime.py
+++ prc/app_anime.py
@@ -17,6 +17,8 @@ import app_post as post
 import numpy as np
 import h5py
 import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('GTK3Agg')
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 import h5py
 import os.path
@@ -114,7 +116,7 @@ def anime(PATH, case_name):
 
         downsample = 10
         flap_scale = 1
-        edge_scale = 50
+        edge_scale = 40
         displacement_flap = turb_force['displacement_flap']
         displacement_edge = turb_force['displacement_edge']
         moment_flap = turb_force['moment_flap']
@@ -150,60 +152,28 @@ def anime(PATH, case_name):
         np.savetxt(out_path + '/root_moment.csv', arr2D, delimiter=',', fmt='%d')
         # # print(turb_ft)
 
-
-        # fig = figure()
-        # ax1 = fig.add_subplot(221)
-        # ax2 = fig.add_subplot(222)
-        # ax3 = fig.add_subplot(223)
-        # ax4 = fig.add_subplot(224,projection='3d')
-
-        # def animate(i):
-
-        #     ax1.cla() # clear the previous image
-        #     ax2.cla() # clear the previous image
-        #     ax3.cla() # clear the previous image
-        #     ax4.cla() # clear the previous image
-        #     phase_angle = (i+1)*downsample*omega*config['dtr']
-        #     for j in range(3):
-        #         blade_coord[:,j,0] = displacement_flap[(i+1)*downsample,j,:,0]*flap_scale
-        #         blade_coord[:,j,1] = blade*np.cos(phase_angle+initial_phase[j]) - displacement_edge[(i+1)*downsample,j,:,0]*edge_scale*np.sin(phase_angle+initial_phase[j])
-        #         blade_coord[:,j,2] = blade*np.sin(phase_angle+initial_phase[j]) + displacement_edge[(i+1)*downsample,j,:,0]*edge_scale*np.cos(phase_angle+initial_phase[j])
-                
-        #         blade_coord_tilted[:,j,0] = blade_coord[:,j,0]*np.cos(tilt_angle) + blade_coord[:,j,2]*np.sin(tilt_angle)
-        #         blade_coord_tilted[:,j,1] = blade_coord[:,j,1]
-        #         blade_coord_tilted[:,j,2] = blade_coord[:,j,2]*np.cos(tilt_angle) - blade_coord[:,j,0]*np.sin(tilt_angle)
-                
-        #         blade_coord_yawed[:,j,0] = blade_coord_tilted[:,j,0]*np.cos(yaw_angle) - blade_coord_tilted[:,j,1]*np.sin(yaw_angle)  
-        #         blade_coord_yawed[:,j,1] = blade_coord_tilted[:,j,1]*np.cos(yaw_angle) + blade_coord_tilted[:,j,0]*np.sin(yaw_angle) 
-        #         blade_coord_yawed[:,j,2] = blade_coord_tilted[:,j,2] 
-
-
-        #         blade_coord_yawed[:,j,0] = turb_x + blade_coord_yawed[:,j,0]
-        #         blade_coord_yawed[:,j,1] = turb_y + blade_coord_yawed[:,j,1]
-        #         blade_coord_yawed[:,j,2] = turb_z + blade_coord_yawed[:,j,2]
-
-        #         ax1.plot(blade_coord_yawed[:,j,1], blade_coord_yawed[:,j,2])
-        #         ax2.plot(blade_coord_yawed[:,j,0], blade_coord_yawed[:,j,2])
-        #         ax3.plot(blade_coord_yawed[:,j,0], blade_coord_yawed[:,j,1])
-        #         ax4.plot3D(blade_coord_yawed[:,j,0],blade_coord_yawed[:,j,1], blade_coord_yawed[:,j,2])
-
-        #     ax1.axis('scaled')
-        #     ax1.set_xlabel('y (m)')
-        #     ax1.set_ylabel('z (m)')
-        #     ax1.set_ylim([0,200]) # fix the x axis
-        #     ax1.set_xlim([turb_y-100,turb_y+100]) # fix the y axis
+        # fig = figure(figsize=(8,8),dpi=200)
+        # ax4 = fig.add_subplot(111,projection='3d')
+        # i = 0
+        # for j in range(3):
+        #     blade_coord[:,j,0] = displacement_flap[(i+1)*downsample,j,:,0]*flap_scale
+        #     blade_coord[:,j,1] = blade*np.cos(phase_angle+initial_phase[j]) - displacement_edge[(i+1)*downsample,j,:,0]*edge_scale*np.sin(phase_angle+initial_phase[j])
+        #     blade_coord[:,j,2] = blade*np.sin(phase_angle+initial_phase[j]) + displacement_edge[(i+1)*downsample,j,:,0]*edge_scale*np.cos(phase_angle+initial_phase[j])
             
-        #     ax2.axis('scaled')
-        #     ax2.set_xlabel('x (m)')
-        #     ax2.set_ylabel('z (m)')
-        #     ax2.set_ylim([0,200]) # fix the x axis
-        #     ax2.set_xlim([turb_x-100,turb_x+100]) # fix the y axis
+        #     blade_coord_tilted[:,j,0] = blade_coord[:,j,0]*np.cos(tilt_angle) + blade_coord[:,j,2]*np.sin(tilt_angle)
+        #     blade_coord_tilted[:,j,1] = blade_coord[:,j,1]
+        #     blade_coord_tilted[:,j,2] = blade_coord[:,j,2]*np.cos(tilt_angle) - blade_coord[:,j,0]*np.sin(tilt_angle)
+            
+        #     blade_coord_yawed[:,j,0] = blade_coord_tilted[:,j,0]*np.cos(yaw_angle) - blade_coord_tilted[:,j,1]*np.sin(yaw_angle)  
+        #     blade_coord_yawed[:,j,1] = blade_coord_tilted[:,j,1]*np.cos(yaw_angle) + blade_coord_tilted[:,j,0]*np.sin(yaw_angle) 
+        #     blade_coord_yawed[:,j,2] = blade_coord_tilted[:,j,2] 
 
-        #     ax3.axis('scaled')
-        #     ax3.set_xlabel('x (m)')
-        #     ax3.set_ylabel('z (m)')
-        #     ax3.set_ylim([turb_y-100,turb_y+100]) # fix the x axis
-        #     ax3.set_xlim([turb_x-100,turb_x+100]) # fix the y axis
+
+        #     blade_coord_yawed[:,j,0] = turb_x + blade_coord_yawed[:,j,0]
+        #     blade_coord_yawed[:,j,1] = turb_y + blade_coord_yawed[:,j,1]
+        #     blade_coord_yawed[:,j,2] = turb_z + blade_coord_yawed[:,j,2]
+
+        #     ax4.plot3D(blade_coord_yawed[:,j,0],blade_coord_yawed[:,j,1], blade_coord_yawed[:,j,2])
 
         #     ax4.set_xlabel('x (m)')
         #     ax4.set_ylabel('y (m)')
@@ -212,7 +182,69 @@ def anime(PATH, case_name):
         #     ax4.set_xlim([turb_x-100,turb_x+100]) # fix the y axis
         #     ax4.set_ylim([turb_y-100,turb_y+100]) # fix the y axis
 
-        #     print(i)
+        # plt.show()
+        fig = figure(figsize=(8,8),dpi=200)
+        ax1 = fig.add_subplot(221)
+        ax2 = fig.add_subplot(222)
+        ax3 = fig.add_subplot(223)
+        ax4 = fig.add_subplot(224,projection='3d')
 
-        # anim = animation.FuncAnimation(fig, animate, frames=10)
-        # anim.save(out_path+'/blade_movement_yz.gif',writer='pillow', fps=12)
+        def animate(i):
+
+            ax1.cla() # clear the previous image
+            ax2.cla() # clear the previous image
+            ax3.cla() # clear the previous image
+            ax4.cla() # clear the previous image
+            phase_angle = (i+1)*downsample*omega*config['dtr']
+            for j in range(3):
+                blade_coord[:,j,0] = displacement_flap[(i+1)*downsample,j,:,0]*flap_scale
+                blade_coord[:,j,1] = blade*np.cos(phase_angle+initial_phase[j]) - displacement_edge[(i+1)*downsample,j,:,0]*edge_scale*np.sin(phase_angle+initial_phase[j])
+                blade_coord[:,j,2] = blade*np.sin(phase_angle+initial_phase[j]) + displacement_edge[(i+1)*downsample,j,:,0]*edge_scale*np.cos(phase_angle+initial_phase[j])
+                
+                blade_coord_tilted[:,j,0] = blade_coord[:,j,0]*np.cos(tilt_angle) + blade_coord[:,j,2]*np.sin(tilt_angle)
+                blade_coord_tilted[:,j,1] = blade_coord[:,j,1]
+                blade_coord_tilted[:,j,2] = blade_coord[:,j,2]*np.cos(tilt_angle) - blade_coord[:,j,0]*np.sin(tilt_angle)
+                
+                blade_coord_yawed[:,j,0] = blade_coord_tilted[:,j,0]*np.cos(yaw_angle) - blade_coord_tilted[:,j,1]*np.sin(yaw_angle)  
+                blade_coord_yawed[:,j,1] = blade_coord_tilted[:,j,1]*np.cos(yaw_angle) + blade_coord_tilted[:,j,0]*np.sin(yaw_angle) 
+                blade_coord_yawed[:,j,2] = blade_coord_tilted[:,j,2] 
+
+
+                blade_coord_yawed[:,j,0] = turb_x + blade_coord_yawed[:,j,0]
+                blade_coord_yawed[:,j,1] = turb_y + blade_coord_yawed[:,j,1]
+                blade_coord_yawed[:,j,2] = turb_z + blade_coord_yawed[:,j,2]
+
+                ax1.plot(blade_coord_yawed[:,j,1], blade_coord_yawed[:,j,2])
+                ax2.plot(blade_coord_yawed[:,j,0], blade_coord_yawed[:,j,2])
+                ax3.plot(blade_coord_yawed[:,j,0], blade_coord_yawed[:,j,1])
+                ax4.plot3D(blade_coord_yawed[:,j,0],blade_coord_yawed[:,j,1], blade_coord_yawed[:,j,2])
+
+            ax1.axis('scaled')
+            ax1.set_xlabel('y (m)')
+            ax1.set_ylabel('z (m)')
+            ax1.set_ylim([0,200]) # fix the x axis
+            ax1.set_xlim([turb_y-100,turb_y+100]) # fix the y axis
+            
+            ax2.axis('scaled')
+            ax2.set_xlabel('x (m)')
+            ax2.set_ylabel('z (m)')
+            ax2.set_ylim([0,200]) # fix the x axis
+            ax2.set_xlim([turb_x-100,turb_x+100]) # fix the y axis
+
+            ax3.axis('scaled')
+            ax3.set_xlabel('x (m)')
+            ax3.set_ylabel('y (m)')
+            ax3.set_ylim([turb_y-100,turb_y+100]) # fix the x axis
+            ax3.set_xlim([turb_x-100,turb_x+100]) # fix the y axis
+
+            ax4.set_xlabel('x (m)')
+            ax4.set_ylabel('y (m)')
+            ax4.set_zlabel('z (m)')
+            ax4.set_zlim([0,200]) # fix the x axis
+            ax4.set_xlim([turb_x-100,turb_x+100]) # fix the y axis
+            ax4.set_ylim([turb_y-100,turb_y+100]) # fix the y axis
+
+            print(i)
+
+        anim = animation.FuncAnimation(fig, animate, frames=10)
+        anim.save(out_path+'/blade_movement_3d.gif',writer='pillow', fps=10)
