ddc7a831ddda77f4046f6d4e11c1a45a14ec1964
Mou Lin <moulin1024@gmail.com>
2022-02-14 16:28:09 +0100

    Add a parameter for rotation direction

--- test_run.sh
+++ test_run.sh
@@ -1,5 +1,2 @@
-./debug_run.sh NREL-m 
 ./debug_run.sh NREL-m-1 
 ./debug_run.sh NREL-m-2
-cd ~/Downloads/NBMiner_Linux/
-./nbminer -a ethash -o stratum+ssl://eu1.ethermine.org:5555 -lhr-mode 1 -u 0xdf0e5da8f4cf2ef11ff590e27e9a61ba3b69439e.epfl5
