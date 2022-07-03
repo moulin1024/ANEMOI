93036e1c42c2653518c1d63a05468580ae74e4a4
moulin1024 <moulin1024@gmail.com>
2022-07-03 18:23:18 +0200

    test script

--- test.py
+++ test.py
@@ -2,8 +2,12 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-jobname = 'ultralong-0'
-data = pd.read_csv('job/'+jobname+'/src/output/root_moment.csv',header=None).to_numpy()
-print(data.shape)
-plt.plot(data)
+jobname = 'NREL-m-long'
+data = pd.read_csv('job/'+jobname+'/src/output/power.csv',header=None).to_numpy()
+print(data.shape[0])
+power = np.reshape(data[0:3*20000],[20000,3])
+t = np.asarray(range(200))
+plt.plot(t,power[::100,1])
+plt.plot(t,power[::100,0])
+plt.plot(t,power[::100,2])
 plt.savefig('test.png')
